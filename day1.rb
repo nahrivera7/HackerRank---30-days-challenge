1b152ab7a54d4821f35b6a76245a2c86097dde4e
Natalia <nahrivera7@gmail.com>
2019-04-15 16:11:53 -0500

    Day 1: Data types

---
+++ day1.rb
@@ -0,0 +1,13 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+
+first_line = gets.strip.to_i
+second_line = gets.strip.to_f.round(1)
+third_line = gets.chomp
+
+puts i + first_line
+
+puts d + second_line
+
+puts s + third_line
