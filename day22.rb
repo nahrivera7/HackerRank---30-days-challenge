9b84227ac8d71e6382ea5559e8550dd9ada17dd0
Natalia <nahrivera7@gmail.com>
2019-05-01 18:15:52 -0500

    Binary search trees

---
+++ day22.rb
@@ -0,0 +1,41 @@
+class Node
+  attr_accessor :left,:right,:data
+  def initialize data
+    @left=@right=nil
+    @data=data
+  end
+end
+class Solution
+  def insert (root,data)
+    if root==nil
+      return Node.new(data)
+    else
+      if data<=root.data
+        cur=self.insert(root.left,data)
+        root.left=cur
+      else
+        cur=self.insert(root.right,data)
+        root.right=cur
+      end
+    end
+    return root
+  end
+
+  def getHeight(root)
+    if root == nil
+      height = -1
+    else
+      height = 1 + [getHeight(root.left), getHeight(root.right)].max
+    end
+  end
+end
+
+myTree=Solution.new
+root=nil
+T=gets.to_i
+for i in 1..T
+  data=gets.to_i
+  root=myTree.insert(root,data)
+end
+height=myTree.getHeight(root)
+print height
